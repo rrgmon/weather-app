1e24ec7511fcfc4614523fe706c381a6d0371be9
rrgmon <rrgmon@protonmail.com>
2020-02-06 14:41:13 +0530

    Fixed proxy

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   let long;
   let lat;
   let temperatureDescription = document.querySelector(
-    '.temperature-description'
+    ".temperature-description"
   );
-  let temperatureDegree = document.querySelector('.temperature-degree');
-  let locationTimezone = document.querySelector('.location-timezone');
-  let degreeSection = document.querySelector('.degree-section');
-  const temperatureSpan = document.querySelector('.degree-section span');
+  let temperatureDegree = document.querySelector(".temperature-degree");
+  let locationTimezone = document.querySelector(".location-timezone");
+  let degreeSection = document.querySelector(".degree-section");
+  const temperatureSpan = document.querySelector(".degree-section span");
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
 
-      const proxy = `http://cors-anywhere.herokuapp.com/`;
-      const api = `${proxy}https://api.darksky.net/forecast/d0e2c055794e37d675ce6e64dffdbc85/${lat},${long}`;
+      // const proxy = `http://cors-anywhere.herokuapp.com/`;
+      const api = `https://api.darksky.net/forecast/d0e2c055794e37d675ce6e64dffdbc85/${lat},${long}`;
 
       fetch(api)
         .then(response => {
@@ -30,18 +30,18 @@ window.addEventListener('load', () => {
           locationTimezone.textContent = data.timezone;
 
           //Set Icon
-          setIcons(icon, document.querySelector('.icon'));
+          setIcons(icon, document.querySelector(".icon"));
 
           //Degree Calculation
           let celsius = (temperature - 32) * (5 / 9);
 
           //Change temperature to C/F
-          degreeSection.addEventListener('click', () => {
-            if (temperatureSpan.textContent === '°F') {
-              temperatureSpan.textContent = '°C';
+          degreeSection.addEventListener("click", () => {
+            if (temperatureSpan.textContent === "°F") {
+              temperatureSpan.textContent = "°C";
               temperatureDegree.textContent = Math.floor(celsius);
             } else {
-              temperatureSpan.textContent = '°F';
+              temperatureSpan.textContent = "°F";
               temperatureDegree.textContent = temperature;
             }
           });
@@ -50,8 +50,8 @@ window.addEventListener('load', () => {
   }
 
   function setIcons(icon, iconID) {
-    const skycons = new Skycons({ color: 'white' });
-    const currentIcon = icon.replace(/-/g, '_').toUpperCase();
+    const skycons = new Skycons({ color: "white" });
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
     skycons.play();
     return skycons.set(iconID, Skycons[currentIcon]);
   }
